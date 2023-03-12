6dff5b6f1c72ecab77ece69d53d9266efcebc4e2
Syed Hassan <sai-yed@outlook.com>
2023-03-12 12:49:18 -0400

    with all the test graphs

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -21,36 +21,67 @@
 # change the_raw_data to whatever name you assigned when you downloaded it.
 
 
-remotes::install_github("kjhealy/gssr")
-
-if (!require("drat")) {
-  install.packages("drat")
-  library("drat")
-}
-
-
-drat::addRepo("kjhealy")
-
-library(gssr)
+library(foreign)
+library(here)
+library(readr)
+library(dplyr)
 library(tidyverse)
+library(readxl)
 
-gss18 <- gss_get_yr(2018)
 
-gss18
+xl_data = read_excel('inputs/data/GSS/GSS.xlsx')
 
-data(gss_all)
+clean_data <- xl_data |> 
+  select(year, id_, age, degree, sex, race) |> 
+  filter(year > 1997) |> 
+  filter(year < 2019)
 
-gss_all
 
-data(gss_doc)
-gss_doc
+ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point()
 
-gss_doc %>% filter(id == "degree") %>% 
-  select(id, description, text)
+? SELECT
 
-test_data_id <- gss_doc[[3]][[1]]
+# GSS_metadata <- read.dct(here::here("inputs/data/GSS/GSS.dct"))
+# GSS_ascii <- read.dat(here::here("inputs/data/GSS/GSS.dat"), GSS_metadata)
+# attr(GSS_ascii, "col.label") <- GSS_metadata[["ColLabel"]]
+# GSS <- GSS_ascii |>
+#   select(BALLOT, HAPPY, GIVHMLSS, DIRECTNS, YEAR)
+# 
+# write_csv(
+#   x = GSS,
+#   file = here::here("inputs/data/raw_data.csv"),
+# )
 
-test_data_01 <- gss_get_marginals(varnames = c("degree", "race", "sex"))
+# remotes::install_github("kjhealy/gssr")
+# 
+# if (!require("drat")) {
+#   install.packages("drat")
+#   library("drat")
+# }
+# 
+# 
+# drat::addRepo("kjhealy")
+# 
+# library(gssr)
+# library(tidyverse)
+# 
+# gss18 <- gss_get_yr(2018)
+# 
+# gss18
+# 
+# data(gss_all)
+# 
+# gss_all
+# 
+# data(gss_doc)
+# gss_doc
+# 
+# gss_doc %>% filter(id == "degree") %>%
+#   select(id, description, text)
+# 
+# test_data_id <- gss_doc[[3]][[1]]
+# 
+# test_data_01 <- gss_get_marginals(varnames = c("degree", "race", "sex"))
 
 # gss_doc |>
 #  gss_get_props(varnames = c("id", "age", "degree", "sex", "race", "region"))
