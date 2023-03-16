2816f57839003b93c9cf6e6f747de74ab6f6e1fb
Syed Hassan <sai-yed@outlook.com>
2023-03-16 06:25:33 -0400

    simulation updated with preamble

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,43 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates education data to sketch the intended dataset
+# Author: Laura Lee-Chu, Will Davidson, and Syed Hassan
+# Data: 16 March 2023
+# Contact: syed.ext@gmail.com
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: R Studio
 
 
 #### Workspace setup ####
-library(tidyverse)
-# [...UPDATE THIS...]
+
+# create a sample column with unique ids
+id_column <- sample(x = 89:98)
+
+# a column with sample year data
+year_column <- sample(x = 2000:2020, size = 10, replace = TRUE)
+
+# a column with degree types
+degree_column <- sample(x = c("High School", "Primary School", "Diploma", "Bachelors", "Masters"), size = 10, replace = TRUE) 
+
+# a column with sex data
+sex_column <- sample(x = c("Male", "Female", "Other"), size = 10, replace = TRUE) 
+
+# a column with sex data
+race_column <- sample(x = c("White", "Black", "Brown", "Native"), size = 10, replace = TRUE) 
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+
+# creates a data frame with column names being populated with the respective column variables
+simulation_01 <- data.frame("id" = id_column,
+                            "year" = year_column,
+                            "degree" = degree_column,
+                            "sex" = sex_column,
+                            "race" = race_column)
+
+# calls in the data frame
+simulation_01
+
+
 
 
 
